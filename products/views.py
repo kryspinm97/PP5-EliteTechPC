e0af839d62596e8c368e9aa3b20bb5552a7f8ccb
kryspinm97 <maneskryspin69@gmail.com>
2023-06-20 20:33:05 +0000

    Added fixtures for products, added queries for searching / sorting

--- products/views.py
+++ products/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render, get_object_or_404
-from .models import PrebuiltPC
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import PrebuiltPC, Category
+from django.contrib import messages
+from django.db.models import Q
 # Create your views here.
 
 
 def products_prebuiltpc(request):
-
     """ A view to show the list of products along with search and sorting """
-
     products = PrebuiltPC.objects.all()
+    query = request.GET.get('q')
+    category_values = request.GET.getlist('category')
+
+    if query:
+        queries = Q(name__icontains=query) | Q(graphics_card__icontains=query) | Q(processor__icontains=query) | Q(ram__icontains=query) | Q(storage__icontains=query)
+        products = products.filter(queries)
+
+    if category_values:
+        categories = Category.objects.filter(name__in=category_values)
+        products = products.filter(category__in=categories)
 
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories if category_values else None,
     }
 
     return render(request, 'products/products.html', context)
