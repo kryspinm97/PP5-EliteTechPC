4b5394015cd0c26668e2680d9e0942721c1a626c
kryspinm97 <maneskryspin69@gmail.com>
2023-06-20 10:15:37 +0000

    Added product initial template, urls and views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import PrebuiltPC
 # Create your views here.
+
+
+def products_prebuiltpc(request):
+
+    """ A view to show the list of products along with search and sorting """
+
+    products = PrebuiltPC.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
