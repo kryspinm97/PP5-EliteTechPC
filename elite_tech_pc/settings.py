d204e574ab3250c41a6ebf7d1354bae30dd7009b
kryspinm97 <maneskryspin69@gmail.com>
2023-06-20 09:47:37 +0000

    Added products app + models and initial fixtures

--- elite_tech_pc/settings.py
+++ elite_tech_pc/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
+    
 ]
 
 MIDDLEWARE = [
---
+++ products/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class PrebuiltPC(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=100)
+    processor = models.CharField(max_length=100)
+    graphics_card = models.CharField(max_length=100)
+    ram = models.CharField(max_length=100)
+    storage = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
