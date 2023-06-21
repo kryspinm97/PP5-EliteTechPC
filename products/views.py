dffb1413469dc3ac702d84406ac546189fc1bc67
kryspinm97 <maneskryspin69@gmail.com>
2023-06-21 19:33:41 +0000

    Added sorting functionality for products

--- products/views.py
+++ products/views.py
@@ -10,19 +10,42 @@ def products_prebuiltpc(request):
     products = PrebuiltPC.objects.all()
     query = request.GET.get('q')
     category_values = request.GET.getlist('category')
+    sort = 'name'  # Initialize sort with default value
+    direction = 'asc'
+    categories = None
 
     if query:
-        queries = Q(name__icontains=query) | Q(graphics_card__icontains=query) | Q(processor__icontains=query) | Q(ram__icontains=query) | Q(storage__icontains=query)
+        queries = Q(name__icontains=query) | Q(graphics_card__icontains=query) | Q(processor__icontains=query) | Q(
+            ram__icontains=query) | Q(storage__icontains=query)
         products = products.filter(queries)
 
     if category_values:
         categories = Category.objects.filter(name__in=category_values)
         products = products.filter(category__in=categories)
 
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories if category_values else None,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
