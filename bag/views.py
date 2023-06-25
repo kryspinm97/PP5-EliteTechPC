ccdbd422baae219d8c626bd7bed24b67c1f42c27
kryspinm97 <maneskryspin69@gmail.com>
2023-06-25 14:26:05 +0000

    Added the checkout success initial template + logic

--- bag/views.py
+++ bag/views.py
@@ -20,7 +20,8 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
-        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+        messages.success(
+            request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your basket')
@@ -38,27 +39,28 @@ def adjust_bag(request, item_id):
 
     if quantity > 0:
         bag[item_id] = quantity
-        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+        messages.success(
+            request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
         messages.success(request, f'Removed {product.name} from your basket')
-    
+
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
 
 def remove_from_bag(request, item_id):
     """ Remove an item from the shopping bag """
-    
+
     product = get_object_or_404(PrebuiltPC, pk=item_id)
     bag = request.session.get('bag', {})
-    
+
     if item_id in bag:
         del bag[item_id]
         request.session['bag'] = bag
         messages.success(request, f'Removed {product.name} from your basket')
-        return HttpResponse(status=204)  # Return HTTP status 204 (No Content) for successful removal
-    
-    return HttpResponse(status=400)  # Return HTTP status 400 (Bad Request) if item not found in bag
+        # Return HTTP status 204 (No Content) for successful removal
+        return HttpResponse(status=204)
 
-
+    # Return HTTP status 400 (Bad Request) if item not found in bag
+    return HttpResponse(status=400)
--- checkout/signals.py
+++ checkout/signals.py
@@ -5,13 +5,14 @@ from .models import OrderLineItem
 
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
-    """ Update order total on lineitem update/save """
-
+    """
+    Update order total on lineitem update/create
+    """
     instance.order.update_total()
 
-
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance,**kwargs):
-    """ Update order total on lineitem update/save """
-
+def update_on_delete(sender, instance, **kwargs):
+    """
+    Update order total on lineitem delete
+    """
     instance.order.update_total()
