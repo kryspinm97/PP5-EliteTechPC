2723342ff3d3a66dcb2eafb0d4030685a16b97f8
kryspinm97 <maneskryspin69@gmail.com>
2023-06-25 13:00:45 +0000

    Added stripe payment element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51N6YHOKHuRzIDZzhO4QTE3BCpGTLygOY3nG7p7TiOqwGQ6HIouxzlhLYsVR1SOMtdxzjbXQYe8Vax8YOibCBT1P800d4C05ciA',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
