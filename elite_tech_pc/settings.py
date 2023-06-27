82de3ae0fb4e09922b31dbf2a31de505994b5d02
kryspinm97 <maneskryspin69@gmail.com>
2023-06-27 22:04:08 +0000

    Secret key changed + Debug mode adjusted

--- elite_tech_pc/settings.py
+++ elite_tech_pc/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6ube-(^yt0+!%wwv51h8!_pme_!p*f5t4bew(6r2u(!gjq+yi_'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['elite-tech-pc-e966cb86c987.herokuapp.com', 'localhost']
 
