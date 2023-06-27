0c5c55fb1cb4ff581c5406031d6ede9e024d9b80
kryspinm97 <maneskryspin69@gmail.com>
2023-06-27 15:33:17 +0000

    Added Product Admin - Edit Functionality

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.products_prebuiltpc, name='products'),
     path('<int:product_id>/', views.prebuiltpc_details, name='products_details'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
