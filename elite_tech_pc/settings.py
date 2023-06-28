be90b3d0c61a25045889f7da2fbd63b6359067af
kryspinm97 <maneskryspin69@gmail.com>
2023-06-28 18:31:07 +0000

    Cache control Added

--- elite_tech_pc/settings.py
+++ elite_tech_pc/settings.py
@@ -184,6 +184,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
+
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'elite-tech-pc'
     AWS_S3_REGION_NAME = 'eu-west-1'
@@ -200,7 +207,7 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-    
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
