7c7f992bd50cfc660d30711aba6841836ab6daea
kryspinm97 <maneskryspin69@gmail.com>
2023-06-29 11:55:47 +0000

    Media/Static path adjusted

--- elite_tech_pc/settings.py
+++ elite_tech_pc/settings.py
@@ -200,9 +200,9 @@ if 'USE_AWS' in os.environ:
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static/'
+    STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media/'
+    MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
